7bcab3a1073d748a332bc6c94651cb3f75b40a7a
Juan Daniel Galarza Rodriguez <juan.daniel.galarza@hotmail.com>
2020-09-03 15:47:45 -0500

    adding attributes to the pet details page

--- src/pages/pet-details/pet-details.component.jsx
+++ src/pages/pet-details/pet-details.component.jsx
@@ -9,6 +9,8 @@ import { Carousel } from "react-responsive-carousel";
 
 import "./pets-details-page.scss";
 
+import { IoIosCheckmark, IoIosClose } from "react-icons/io";
+
 /**
  * Component that display the details of a pet in a page
  */
@@ -44,6 +46,32 @@ class PetsDetails extends Component {
             {details.breeds.primary} &mdash; {details.gender}
           </p>
           <p className="description">{details.description}</p>
+          <div className="environment-container">
+            <div className="attribute">
+              {details.attributes.house_trained ? (
+                <IoIosCheckmark className="status" />
+              ) : (
+                <IoIosClose className="status" />
+              )}
+              <p>House trained</p>
+            </div>
+            <div className="attribute">
+              {details.attributes.spayed_neutered ? (
+                <IoIosCheckmark className="status" />
+              ) : (
+                <IoIosClose className="status" />
+              )}
+              <p className="name">Spayed neutered</p>
+            </div>
+            <div className="attribute">
+              {details.attributes.special_needs ? (
+                <IoIosClose className="status" />
+              ) : (
+                <IoIosClose className="status" />
+              )}
+              <p className="name">Special needs</p>
+            </div>
+          </div>
         </div>
       </div>
     );
