9d1c47a98d1115dcf2107b136e35ef50a48ba6d6
Juan Daniel Galarza Rodriguez <juan.daniel.galarza@hotmail.com>
2020-09-03 20:45:38 -0500

    adding status info to the pet details page

--- src/pages/pet-details/pet-details.component.jsx
+++ src/pages/pet-details/pet-details.component.jsx
@@ -39,11 +39,13 @@ class PetsDetails extends Component {
               </div>
             ))}
           </Carousel>
+          <p className="status">{details.status.toUpperCase()}</p>
         </div>
         <div className="details-container">
           <h1 className="title">{details.name}</h1>
           <p className="breed-and-location">
-            {details.breeds.primary} &mdash; {details.gender}
+            {details.breeds.primary} &mdash; {details.gender} &mdash;{" "}
+            {details.age} &mdash; {details.size}
           </p>
           {details.tags.length > 0 ? (
             <div className="tags-container">
@@ -53,33 +55,69 @@ class PetsDetails extends Component {
             </div>
           ) : null}
 
-          <p className="description">{details.description}</p>
-          <div className="environment-container">
-            <div className="attribute">
-              {details.attributes.house_trained ? (
-                <IoIosCheckmark className="status" />
-              ) : (
-                <IoIosClose className="status" />
-              )}
-              <p>House trained</p>
+          <p className="description">
+            {details.description ? details.description : null}
+          </p>
+          <div className="detail-lists-container">
+            <div className="environment-container">
+              <div className="attribute">
+                {details.attributes.house_trained ? (
+                  <IoIosCheckmark className="status" />
+                ) : (
+                  <IoIosClose className="status" />
+                )}
+                <p>House trained</p>
+              </div>
+              <div className="attribute">
+                {details.attributes.spayed_neutered ? (
+                  <IoIosCheckmark className="status" />
+                ) : (
+                  <IoIosClose className="status" />
+                )}
+                <p className="name">Spayed neutered</p>
+              </div>
+              <div className="attribute">
+                {details.attributes.special_needs ? (
+                  <IoIosClose className="status" />
+                ) : (
+                  <IoIosClose className="status" />
+                )}
+                <p className="name">Special needs</p>
+              </div>
             </div>
-            <div className="attribute">
-              {details.attributes.spayed_neutered ? (
-                <IoIosCheckmark className="status" />
-              ) : (
-                <IoIosClose className="status" />
-              )}
-              <p className="name">Spayed neutered</p>
-            </div>
-            <div className="attribute">
-              {details.attributes.special_needs ? (
-                <IoIosClose className="status" />
-              ) : (
-                <IoIosClose className="status" />
-              )}
-              <p className="name">Special needs</p>
+            <div className="environment-container">
+              <div className="attribute">
+                {details.environment.cats ? (
+                  <IoIosCheckmark className="status" />
+                ) : (
+                  <IoIosClose className="status" />
+                )}
+                <p>Cats</p>
+              </div>
+              <div className="attribute">
+                {details.environment.children ? (
+                  <IoIosCheckmark className="status" />
+                ) : (
+                  <IoIosClose className="status" />
+                )}
+                <p className="name">Children</p>
+              </div>
+              <div className="attribute">
+                {details.environment.dogs ? (
+                  <IoIosClose className="status" />
+                ) : (
+                  <IoIosClose className="status" />
+                )}
+                <p className="name">Dogs</p>
+              </div>
             </div>
           </div>
+          <p className="status-text">
+            This pet is ready to be adopted, if you want more info please click
+            <a className="text-button" href={details.url} target="_blank">
+              here
+            </a>
+          </p>
         </div>
       </div>
     );
