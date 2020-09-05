17e1685d271b8a6f7c759a7c246523b6d50071f6
Juan Daniel Galarza Rodriguez <juan.daniel.galarza@hotmail.com>
2020-09-04 21:56:53 -0500

    Adding test for sagas

---
+++ src/redux/pets/pets.sagas.test.js
@@ -0,0 +1,26 @@
+import * as petfinderApi from "../../petfinder-api/pets";
+import { getAnimalTypes } from "./pets.sagas";
+import { runSaga } from "redux-saga";
+
+describe("sagas test suite", () => {
+  describe("getAnimalTypes function", () => {
+    it("should call api and dispatch success action", async () => {
+      const typesMock = {
+        data: [{ name: "type1" }, { name: "type2" }, { name: "type3" }],
+      };
+      const getAnimalTypesAPI = jest
+        .spyOn(petfinderApi, "getAnimalTypesAPI")
+        .mockImplementation(() => Promise.resolve(typesMock));
+      const dispatched = [];
+      const result = await runSaga(
+        {
+          dispatch: (action) => dispatched.push(action),
+        },
+        getAnimalTypes
+      );
+
+      expect(getAnimalTypesAPI).toHaveBeenCalledTimes(1);
+      getAnimalTypesAPI.mockClear();
+    });
+  });
+});
