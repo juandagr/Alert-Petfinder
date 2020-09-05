d9393d28802fe13bacf69a06d7abfed35b158770
Juan Daniel Galarza Rodriguez <juan.daniel.galarza@hotmail.com>
2020-09-04 22:39:59 -0500

    Updates

---
+++ precache-manifest.cdec2ed4040ce3283a016985c41c8b39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "baf6cabf1ea14938abab89b177831a3a",
+    "url": "https//juandagr.github.io/Alert-Petfinder/static/media/photo.baf6cabf.png"
+  },
+  {
+    "revision": "7d57d393a901704be8ddd75f3196b93c",
+    "url": "https//juandagr.github.io/Alert-Petfinder/static/media/krista-mangulsone-9gz3wfHr65U-unsplash.7d57d393.jpg"
+  },
+  {
+    "revision": "cbc940e444593750ada3",
+    "url": "https//juandagr.github.io/Alert-Petfinder/static/js/runtime~main.7f432530.js"
+  },
+  {
+    "revision": "f170d29abd2a360e552f",
+    "url": "https//juandagr.github.io/Alert-Petfinder/static/js/main.f98b0f6a.chunk.js"
+  },
+  {
+    "revision": "a22d6bed13f0f86889e3",
+    "url": "https//juandagr.github.io/Alert-Petfinder/static/js/2.1d03e15f.chunk.js"
+  },
+  {
+    "revision": "f170d29abd2a360e552f",
+    "url": "https//juandagr.github.io/Alert-Petfinder/static/css/main.6c3cb119.chunk.css"
+  },
+  {
+    "revision": "a22d6bed13f0f86889e3",
+    "url": "https//juandagr.github.io/Alert-Petfinder/static/css/2.2b8b969f.chunk.css"
+  },
+  {
+    "revision": "32ab01ab4fc7086b731e0bdf408d0921",
+    "url": "https//juandagr.github.io/Alert-Petfinder/index.html"
+  }
+];
